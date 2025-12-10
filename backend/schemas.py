c6437cf9e2c4781168de433996298bdee42e7e32
ujjwalsinha918 <ujjwalsinha@fxis.ai>
2025-12-10 15:59:26 +0530

    Initial commit

---
+++ backend/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+ 
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"    
+
+   
+            
